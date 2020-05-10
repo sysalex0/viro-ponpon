a8262b91f24f6d965c083279dff724a10b857257
sysalex0@gmail.com <Sysalex1>
2020-05-11 05:06:12 +0800

    change to html, just save in git

--- src/components/BubbleChart/BubbleChart.js
+++ src/components/BubbleChart/BubbleChart.js
@@ -13,14 +13,14 @@ export default class BubbleChart extends React.Component {
         console.log('Bubble:', d)
     }
 
+
     async componentDidMount() {
         const raw_data = await d3.csv(bubbleChartData);
-        const data = raw_data.map(d => ({id:d['ID'], name: d['OP_CARRIER'], value: d['SUM'], airline: d['AIRLINE']}))
+        const data = raw_data.map(d => ({id: d['ID'], name: d['OP_CARRIER'], value: d['SUM'], airline: d['AIRLINE']}))
         const json = {'children': data}
         const diameter = 600
-        // const color = d3.scaleOrdinal(d3.schemeCategory10)
         const color = d3.scaleSequential()
-            .domain([0,data.length-1])
+            .domain([0, data.length - 1])
             .interpolator(d3.interpolateViridis);
 
         const bubble = d3.pack()
@@ -32,6 +32,24 @@ export default class BubbleChart extends React.Component {
             top: 0,
             bottom: 0
         }
+
+        ///////////////////////////////////////////////////////////////////////////
+        const tooltip = d3.select(this.div)
+            .append("div")
+            .attr("class", "tooltip")
+            .style("visibility", "hidden")
+            .style("background-color", "rgba(0, 0, 0, 0.75)")
+            .style("color", "white")
+            .style("position", "absolute")
+            .style("padding", "8px")
+            .style("z-index", "10")
+            .style("border-radius", "6px")
+            .style("font", "12px sans-serif")
+
+        const handleBubbleOnClick = (d) => {
+            console.log('Bubble:', d)
+        }
+
         const svg = d3.select(this.div).append('svg')
             .attr('viewBox', `0 0 ${(diameter + margin.right)} ${diameter}`)
             .attr('width', (diameter + margin.right))
@@ -60,14 +78,28 @@ export default class BubbleChart extends React.Component {
             .attr("r", function (d) {
                 return d.r;
             })
+            .attr("class", "circle")
             .style("fill", function (d) {
-                console.log(color(d.data.id))
+                // console.log(color(d.data.id))
                 return color(d.data.id);
             })
             .on("click", (d) => {
-                this.handleBubbleOnClick(d)
+                handleBubbleOnClick(d)
             })
-
+            .on("mouseover", function (d) {
+                d3.select(this)
+                    .attr("r", d.r + 2);
+                tooltip.text(`${d.data.name} : ${d.value} flights`);
+                tooltip.style("visibility", "visible");
+            })
+            .on("mousemove", function () {
+                return tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px");
+            })
+            .on("mouseout", function (d) {
+                d3.select(this)
+                    .attr("r", d.r - 2);
+                return tooltip.style("visibility", "hidden");
+            });
 
         node.append("text")
             .attr("dy", ".3em")
@@ -75,15 +107,31 @@ export default class BubbleChart extends React.Component {
             .text(function (d) {
                 return d.data.name;
             })
-            .style("fill", "#ffffff");
+            .style("fill", "#ffffff")
+            .on("mouseover", function (d) {
+                d3.select(this)
+                    .attr("r", d.r + 2)
+                    .style("cursor", "default");
+                tooltip.text(d.data.name + ": " + d.value);
+                tooltip.style("visibility", "visible");
+            })
+            .on("mousemove", function () {
+                return tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px");
+            })
+            .on("mouseout", function (d) {
+                d3.select(this)
+                    .attr("r", d.r - 2)
+                    .style("cursor", "default");
+                return tooltip.style("visibility", "hidden");
+            });
 
-
+/////////////////////////////////////////////////////////////////////////
         const legend = svg.selectAll(".legend")
             .data(data).enter()
             .append("g")
             .attr("class", "legend")
             .attr("transform", "translate(" + 780 + "," + 120 + ")");
-        // .attr("transform", "translate(" + 0 + "," + 120 + ")");
+// .attr("transform", "translate(" + 0 + "," + 120 + ")");
 
 
         legend.append("rect")
@@ -102,13 +150,30 @@ export default class BubbleChart extends React.Component {
             .attr("x", 25)
             .attr("text-anchor", "start")
             .attr("dy", "1em")
+            .attr("font-size", "12px")
             .attr("y", function (d, i) {
                 return 20 * i;
             })
             .text(function (d) {
                 return `${d.name} : ${d.airline}`;
             })
-            .attr("font-size", "12px");
+            .on("mouseover", function (d) {
+                const circle = d3.selectAll('circle').filter((data) => data.data.name == d.name)
+                circle
+                    .attr("r", (curCir) => {
+                        return curCir.r + 2
+                    })
+                    .style("cursor", "default");
+            })
+            .on("mouseout", function (d) {
+                d3.select(this)
+                const circle = d3.selectAll('circle').filter((data) => data.data.name == d.name)
+                circle
+                    .attr("r", (curCir) => {
+                        return curCir.r - 2
+                    })
+                    .style("cursor", "default");
+            });
 
 
         legend.append("text")
@@ -117,7 +182,6 @@ export default class BubbleChart extends React.Component {
             .attr("y", -10)
             .text("Airlines")
             .attr("font-size", "17px");
-
         // as our fake div is not using state, so react wont know it is updated
         // we need call force update to make the div render
         this.forceUpdate();
